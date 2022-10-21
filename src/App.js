e76674e215de801e8c77498cc2b5a235539d95f2
Luiz Marangoni <lzmarangoni@gmail.com>
2022-10-21 15:15:30 -0300

    sincronizado a API para envio e recebimento dos dados cadastrados, alem de renderizar de forma dinamica as novas tarefas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Button from './components/Button';
 import Form from './components/Form';
 import Container from './components/Container'
 
-const API = 'http://localhost:5000'
+const API = 'http://localhost:5000/'
 
 function App() {
   const [title, setTitle] = useState('')
@@ -15,14 +15,49 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [loadind, setLoading]= useState(false)
 
-  function handleSubmit(e){
-    e.preventDefault();
-    console.log(title)
-    setTitle('');
+  useEffect(()=>{
+    const loadData = async()=>{
+      setLoading(true);
 
-   
+      const res = await fetch (API + 'tarefas')
+      .then((res)=> res.json())
+      .then((data) => data)
+      .catch((err)=> console.log(err));
+    
+      setLoading(false);
+      setTasks(res)
+    };
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+
+    let newTask = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+
+    await fetch(API+'tarefas',{
+      method:'POST',
+      body: JSON.stringify(newTask),
+      headers: {'Content-Type' :"application/json"},
+    })
+
+    console.log(newTask)
+    
+    setTitle('');
+    setTime('');
+
+    setTasks((prevState)=>[...prevState,newTask])
+
   }
 
+  if(loadind){
+    return <p>Carregando...</p>
+  }
  
 
 
@@ -31,19 +66,29 @@ function App() {
       <Container>
         <Header value="Lista de tarefas"/>
         <Form onSubmit={handleSubmit}>
-          <Input 
-            label={"Nova Tarefa"} 
-            id={"task"}
-            placeholder={"Resuma sua tarefa aqui..."}
-            onChange={(e)=>{setTitle(e.target.value)}}
-            value={title || " "}
-            />
-            
+            <div className='input'>
+              <Input 
+              label={"Nova Tarefa"} 
+              id={"task"}
+              placeholder={"Resuma sua tarefa aqui..."}
+              onChange={(e)=>{setTitle(e.target.value)}}
+              value={title || ""}
+              />
+              <Input 
+              label={"Tempo de realização"} 
+              id={"time"}
+              placeholder={"Qual tempo será necessário (em horas)?"}
+              onChange={(e)=>{setTime(e.target.value)}}
+              value={time || ""}
+              />
+            </div>
+
           <Button label="Criar"/>
         </Form> 
         <div>
           <h3>Tarefas</h3>
           {tasks.length === 0 && <p>Não há tarefas</p>}
+          {tasks.map(task=><p key={task.id}>{task.title}</p>)}
         </div>
       </Container> 
     </div>
